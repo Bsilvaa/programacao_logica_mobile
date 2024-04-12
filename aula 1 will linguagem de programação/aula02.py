c473526b3dc2b10638a31257cb523dcb34bfb5a0
Willy <willysilva136@gmail.com>
2024-04-11 21:49:32 -0300

    update,python
    fiz a atividade do dia 11/04/20224

---
+++ aula 1 will linguagem de programação/aula02.py
@@ -0,0 +1,11 @@
+print("ola mundo,finamlmente quebrei a maldição")
+
+##tudo que estiver entre parentese e aspas vai ser exibido no terminal
+
+print("Ola bem vindo ao nosso app!")
+
+nome=input("qual o seu nome?: ")
+idade=input("qual a sua idade?:")
+gosto=input("qual o seu hobbie ?:")
+
+print(f"olá {nome}, voce tem {idade} anos, gosta de {gosto} as vezes")
